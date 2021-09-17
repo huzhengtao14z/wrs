cf42530ac262428800864f1f23f1ba54a34bdb09
huzhengtao14z <57081627+huzhengtao14z@users.noreply.github.com>
2021-09-17 13:00:28 +0900

    copy hu

--- 0000_examples/grasping_antipodal_planning.py
+++ 0000_examples/grasping_antipodal_planning.py
@@ -5,12 +5,14 @@ import modeling.collision_model as cm
 import grasping.planning.antipodal as gpa
 import robot_sim.end_effectors.grippers.yumi_gripper.yumi_gripper as yg
 
-base = wd.World(cam_pos=[1, 1, 1], lookat_pos=[0, 0, 0])
+base = wd.World(cam_pos=[1, 1, 1],w=960,
+                 h=540, lookat_pos=[0, 0, 0])
 gm.gen_frame().attach_to(base)
 # object
 object_tube = cm.CollisionModel("objects/tubebig.stl")
-object_tube.set_rgba([.9, .75, .35, .3])
+object_tube.set_rgba([.9, .75, .35, 1])
 object_tube.attach_to(base)
+
 # hnd_s
 gripper_s = yg.YumiGripper()
 grasp_info_list = gpa.plan_grasps(gripper_s, object_tube,
@@ -22,5 +24,5 @@ gpa.write_pickle_file('tubebig', grasp_info_list, './', 'yumi_tube_big.pickle')
 for grasp_info in grasp_info_list:
     jaw_width, jaw_center_pos, jaw_center_rotmat, hnd_pos, hnd_rotmat = grasp_info
     gripper_s.grip_at_with_jcpose(jaw_center_pos, jaw_center_rotmat, jaw_width)
-    gripper_s.gen_meshmodel().attach_to(base)
+    gripper_s.gen_meshmodel(rgba=(1,0,0,0.01)).attach_to(base)
 base.run()
