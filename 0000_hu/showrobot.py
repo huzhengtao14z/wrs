eda9390a7c134a50b9b3b2add1ac819be1babdc1
huzhengtao14z <57081627+huzhengtao14z@users.noreply.github.com>
2022-01-06 00:03:14 +0900

    Update showrobot.py

--- 0000_hu/showrobot.py
+++ 0000_hu/showrobot.py
@@ -12,6 +12,8 @@ import basis.robot_math as rm
 import modeling.geometric_model as gm
 import robot_sim.robots.ur3_dual.ur3_dual as ur3d
 import robot_sim.robots.ur3e_dual.ur3e_dual as ur3ed
+import motion.probabilistic.rrt_connect as rrtc
+
 if __name__ == '__main__':
     base = wd.World(cam_pos=[2, 1, 3], w=960,
                     h=540, lookat_pos=[0, 0, 1.1])
@@ -26,13 +28,32 @@ if __name__ == '__main__':
     robot_instance = ur3ed.UR3EDual()
     start_hnd_pos = np.array([0.9, -0.5, 1.3])
     start_hnd_rotmat = rm.rotmat_from_axangle([0, 1, 0], math.pi / 2)
-    goal_hnd_pos = np.array([0.4, -0.3, 1.3])
+    goal_hnd_pos = np.array([0.8, -0.6, 1.3])
     goal_hnd_rotmat = rm.rotmat_from_axangle([0, 1, 0], math.pi / 2)
-    start_jntsangle = robot_instance.ik(component_name,start_hnd_pos,start_hnd_rotmat)
-    robot_instance.fk(component_name, start_jntsangle)
+    start_jntsangle = robot_instance.ik(component_name, start_hnd_pos, start_hnd_rotmat)
+    goal_jntsangle  = robot_instance.ik(component_name, goal_hnd_pos,  goal_hnd_rotmat)
+    # robot_instance.fk(component_name, start_jntsangle)
     # gm.gen_frame(pos=start_hnd_pos, rotmat=start_hnd_rotmat).attach_to(base)
     # gm.gen_frame(pos=goal_hnd_pos, rotmat=goal_hnd_rotmat).attach_to(base)
-    robot_meshmodel = robot_instance.gen_meshmodel(tcp_loc_pos=start_hnd_pos, tcp_loc_rotmat=start_hnd_rotmat)
+    # robot_meshmodel = robot_instance.gen_meshmodel(tcp_loc_pos=start_hnd_pos, tcp_loc_rotmat=start_hnd_rotmat)
 
-    robot_meshmodel.attach_to(base)
+    # robot_meshmodel.attach_to(base)
+    rrtc_planner = rrtc.RRTConnect(robot_instance)
+    path = rrtc_planner.plan(start_conf=start_jntsangle,
+                             goal_conf=goal_jntsangle,
+                             obstacle_list=[object],
+                             ext_dist=.1,
+                             max_time=300,
+                             component_name=component_name)
+
+    for pose in path:
+        # print(pose)
+        robot_instance.fk(component_name, pose)
+        # robot_meshmodel = robot_instance.gen_meshmodel()
+        robot_instance.gen_meshmodel().attach_to(base)
+        # robot_meshmodel.attach_to(base)
+        # robot_meshmodel.show_cdprimit()
+        # robot_instance.gen_stickmodel().attach_to(base)
+
+
     base.run()
